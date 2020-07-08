afbef785b9a34fdfa4546d18ee13349b00401044
chiseungii <43616130+chiseungii@users.noreply.github.com>
2020-07-08 20:01:27 +0900

    Create 02047.cpp

---
+++ 02047.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	string s; cin >> s;
+
+	for (int i = 0; i < s.length(); i++)
+		if (isalpha(s[i]))
+			s[i] = toupper(s[i]);
+
+	cout << s << endl;
+}
